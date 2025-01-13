bbc4801f87380ae0e5e49a0d86bdb7316be0f87f
stho32 <stho32github@gmail.com>
2025-01-13 20:53:26 +0100

    Bitte stelle in der Übrsicht die Tickets in einer Tabelle dar. + Design herunterladen und direkt verknüpfen / relative Links

--- php/includes/footer.php
+++ php/includes/footer.php
@@ -1,4 +1,4 @@
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="<?= $basePath ?>/js/main.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/main.js"></script>
 </body>
 </html>
--- php/includes/header.php
+++ php/includes/header.php
@@ -15,26 +15,26 @@ if (substr($basePath, -8) === '/includes') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
     <meta name="theme-color" content="#007bff">
     <title>SAUS-ES</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?= $basePath ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= $basePath ?>/assets/css/bootstrap-icons.css" rel="stylesheet">
     <link href="<?= $basePath ?>/assets/css/style.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="<?= $basePath ?>/">SAUS-ES</a>
+            <a class="navbar-brand" href="./">SAUS-ES</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= $basePath ?>/index.php">
+                        <a class="nav-link" href="./index.php">
                             <i class="bi bi-list-ul"></i> Übersicht
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= $basePath ?>/create_ticket.php">
+                        <a class="nav-link" href="./create_ticket.php">
                             <i class="bi bi-plus-lg"></i> Neues Ticket
                         </a>
                     </li>
@@ -44,7 +44,7 @@ if (substr($basePath, -8) === '/includes') {
                     <span class="text-light me-3">
                         <i class="bi bi-person"></i> <?= htmlspecialchars(getCurrentUsername()) ?>
                     </span>
-                    <a href="<?= $basePath ?>/logout.php" class="btn btn-outline-light">
+                    <a href="./logout.php" class="btn btn-outline-light">
                         <i class="bi bi-box-arrow-right"></i> Abmelden
                     </a>
                 </div>
