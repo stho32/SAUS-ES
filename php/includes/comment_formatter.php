a22a1faf3eca7da7001d1c8c384130668e5da469
Stefan Hoffmann <stho32github@gmail.com>
2025-01-18 14:27:41 +0100

    schönere Checkboxicons

--- php/includes/comment_formatter.php
+++ php/includes/comment_formatter.php
@@ -17,9 +17,16 @@ function formatComment(string $text): string {
     // Ersetze *text* mit <em>text</em>
     $text = preg_replace('/\*(.*?)\*/s', '<em>$1</em>', $text);
     
-    // Ersetze [x] und [ ] mit Checkboxen
-    $text = preg_replace('/\[x\]/i', '☒', $text);
-    $text = preg_replace('/\[ \]/i', '☐', $text);
+    // Ersetze [x] und [ ] mit Bootstrap Icons
+    $text = preg_replace('/\[x\]/i', '<i class="bi bi-check-square-fill fs-5"></i>', $text);
+    $text = preg_replace('/\[ \]/i', '<i class="bi bi-square fs-5"></i>', $text);
+    
+    // URLs in Links umwandeln
+    $text = preg_replace(
+        '/(https?:\/\/[^\s<]+)/',
+        '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>',
+        $text
+    );
     
     return $text;
 }
