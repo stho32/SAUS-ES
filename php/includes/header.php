5436c7fca4821c8fefb8bd3bf40a2c9df8769613
Stefan Hoffmann <stho32github@gmail.com>
2025-01-21 19:24:20 +0100

    Umbenennung nach SAUSi

--- php/includes/header.php
+++ php/includes/header.php
@@ -14,7 +14,7 @@ if (substr($basePath, -8) === '/includes') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
     <meta name="theme-color" content="#007bff">
-    <title>SAUS-ES</title>
+    <title>SAUS-i</title>
     <link href="<?= $basePath ?>/assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?= $basePath ?>/assets/css/bootstrap-icons.css" rel="stylesheet">
     <link href="<?= $basePath ?>/assets/css/style.css" rel="stylesheet">
@@ -40,7 +40,7 @@ if (substr($basePath, -8) === '/includes') {
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="./">SAUS-ES</a>
+            <a class="navbar-brand" href="./">SAUS-i</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
